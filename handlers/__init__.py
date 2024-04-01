a6c5c193e8f75ea4c99b8114d1314ec2cdf109c7
Vadim <vadim.sodomov@yandex.ru>
2024-04-01 21:16:33 +0300

    emojis added

--- handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-def register_handlers():
-    from . import user
-    from . import add_question
-    from . import delete_question
-    from . import person_stats
-    from . import all_stats
-    from . import get_survey

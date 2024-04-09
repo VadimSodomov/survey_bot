4b1031b845267501b8fec3afaee2499ab8c6264c
VadimSodomov <vadim.sodomov@yandex.ru>
2024-04-09 22:23:54 +0300

    add smilies

---
+++ smilies.py
@@ -0,0 +1,12 @@
+smilies = {
+    "night": "\U0001F303",
+    "morning": "\U0001F304",
+    "day": "\U0001F3D9",
+    "evening": "\U0001F306",
+    "checkbox": "\U00002705",
+    "statistic": "\U0001F4CB",
+    "all_statistic": "\U0001F5C2",
+    "pencil": "\U0000270F",
+    "delete": "\U0000274C",
+    "ok": "\U0001F44C"
+}
